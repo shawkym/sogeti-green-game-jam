addf7d2c8e4609a6d68b614016c4e8f1b27e71c6
Nishchhal <nishchhalbakshi@gmail.com>
2022-06-11 03:00:05 +0200

    Project Structure and initial cod commit.

---
+++ Assets/_Project/Scripts/BoatController.cs
@@ -0,0 +1,111 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoatController : MonoBehaviour
+{
+    [SerializeField] private Transform boatTransform;
+    [SerializeField] private Camera mainCameraRef;
+
+    [SerializeField] private float boatStartXPos;
+    [SerializeField] private float boatTargetXPos;
+    
+    [SerializeField] private float boatMoveSpeed = 10f;
+    [SerializeField] private float timeToMoveToTarget = 0;
+    [SerializeField] private float currentTimeToMoveToTarget = 0;
+
+    [SerializeField] private LineRenderer hookConnectorLine;
+
+    [SerializeField] private Transform hookOriginPoint;
+    [SerializeField] private Transform hookConnectionPoint;
+    [SerializeField] private Hook hook;
+    void Start()
+    {
+        boatTransform = transform;
+        mainCameraRef = Camera.main;
+    }
+    
+    void Update()
+    {
+        GetInput();
+        MoveBoat();
+        HandleHookConnection();
+    }
+
+    private void GetInput()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            HandleMouseInput();
+        }
+
+        if (Input.GetMouseButtonDown(1) && !hook.IsHookProcessing())
+        {
+            HandleHookThrow();
+        }
+    }
+
+    private void HandleMouseInput()
+    {
+        Vector2 mousePosWorld = mainCameraRef.ScreenToWorldPoint(Input.mousePosition);
+        float mouseXPos = mousePosWorld.x;
+        BoatDirection boatDirection = BoatDirection.Left;
+
+        if (mouseXPos < boatTransform.position.x)
+        {
+            //Move Left
+            Debug.Log("Move Left");
+            boatDirection = BoatDirection.Left;
+        }
+
+        else
+        {
+            //Move Right
+            Debug.Log("Move Right");
+            boatDirection = BoatDirection.Right;
+        }
+
+        FlipBoat(boatDirection);
+        boatTargetXPos = mouseXPos;
+        boatStartXPos = boatTransform.position.x;
+        timeToMoveToTarget = Mathf.Abs(mouseXPos - boatTransform.position.x) / boatMoveSpeed;
+        currentTimeToMoveToTarget = 0;
+    }
+
+    private void FlipBoat(BoatDirection boatDirection)
+    {
+        if (boatDirection == BoatDirection.Left)
+        {
+            boatTransform.localScale = new Vector3(-1, 1, 1);
+            return;
+        }
+        
+        boatTransform.localScale = new Vector3(1, 1, 1);
+    }
+
+    private void MoveBoat()
+    {
+        currentTimeToMoveToTarget += Time.deltaTime;
+        float targetXPos = Mathf.Lerp(boatStartXPos, boatTargetXPos, currentTimeToMoveToTarget / timeToMoveToTarget);
+        boatTransform.position = new Vector3(targetXPos, boatTransform.position.y, boatTransform.position.z);
+    }
+
+    private void HandleHookThrow()
+    {
+        hook.Throw();
+    }
+
+    private void HandleHookConnection()
+    {
+        hookConnectorLine.positionCount = 2;
+        hookConnectorLine.SetPosition(0, hookOriginPoint.position);
+        hookConnectorLine.SetPosition(1, hookConnectionPoint.position);
+    }
+}
+
+public enum BoatDirection
+{
+    Left,
+    Right
+}
---
+++ Assets/_Project/Scripts/Hook.cs
@@ -0,0 +1,74 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hook : MonoBehaviour
+{
+    [SerializeField] private float originalYPos;
+    [SerializeField] private float startYPos;
+    [SerializeField] private float targetYPos;
+    
+    [SerializeField] private float timeToMoveToTarget = 2f;
+    [SerializeField] private float currentTimeToMoveToTarget = 0;
+    [SerializeField] private bool throwen = false;
+
+    [SerializeField] private int trashCollectedCount = 0;
+
+    private void Start()
+    {
+        originalYPos = transform.position.y;
+        startYPos = originalYPos;
+        targetYPos = originalYPos;
+    }
+
+    private void Update()
+    {
+        MoveHook();
+        DetectTrash();
+    }
+
+    private void MoveHook()
+    {
+        currentTimeToMoveToTarget += Time.deltaTime;
+        float _targetYPos = Mathf.Lerp(startYPos, targetYPos, currentTimeToMoveToTarget / timeToMoveToTarget);
+        transform.position = new Vector3(transform.position.x, _targetYPos, transform.position.z);
+
+        if (currentTimeToMoveToTarget / timeToMoveToTarget > 1 && throwen)
+        {
+            targetYPos = originalYPos;
+            startYPos = transform.position.y;
+            throwen = false;
+            currentTimeToMoveToTarget = 0;
+        }
+    }
+
+    private void DetectTrash()
+    {
+        Collider2D[] trashCollided = Physics2D.OverlapCircleAll(transform.position, 0.5f);
+        for (int i = 0; i < trashCollided.Length; i++)
+        {
+            Debug.Log(trashCollided[i].name + "Collected");
+            trashCollectedCount++;
+            Destroy(trashCollided[i].gameObject);
+        }
+    }
+
+    public bool IsHookProcessing()
+    {
+        if (currentTimeToMoveToTarget / timeToMoveToTarget > 1 && !throwen)
+        {
+            return false;
+        }
+
+        return true;
+    }
+
+    public void Throw()
+    {
+        currentTimeToMoveToTarget = 0;
+        throwen = true;
+        startYPos = transform.position.y;
+        targetYPos = transform.position.y - 5;
+    }
+}
